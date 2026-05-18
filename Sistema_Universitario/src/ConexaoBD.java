c4775d670a80bcf6799de1d57aa612dfbcad149d
Victor Hugo <vixs1212@gmail.com>
2026-05-18 00:08:47 -0300

    implementando a lógica do projeto, e ocultando os dados do BD

--- Sistema_Universitario/src/ConexaoBD.java
+++ Sistema_Universitario/src/ConexaoBD.java
@@ -1,29 +1,30 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-//conexão do banco de dados e do driver do SQL
-
 public class ConexaoBD {
-	private static final String usuario = "root";
-    private static final String senha = "Arrow1212$";
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/sistema_universitario";
-
-    private static final String Driver = "com.mysql.cj.jdbc.Driver";
     private static Connection conexao = null;
 
-    public static Connection getConnection() throws SQLException{
-    	if(conexao == null || conexao.isClosed()) {
-    		try {
-    			Class.forName(Driver);
-    			conexao = DriverManager.getConnection(URL, usuario, senha);
-    		}catch(ClassNotFoundException e) {
-    			throw new SQLException("Driver não encontrado: " + e.getMessage());
-    		}
- 
-        
-    	}
-    	
-    	return conexao;
+    public static Connection getConnection() throws SQLException {
+        if (conexao == null || conexao.isClosed()) {
+            try {
+                Properties props = new Properties();
+                props.load(new FileInputStream("config.properties"));
+
+                String url = props.getProperty("db.url");
+                String usuario = props.getProperty("db.usuario");
+                String senha = props.getProperty("db.senha");
+
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conexao = DriverManager.getConnection(url, usuario, senha);
+
+            } catch (ClassNotFoundException | IOException e) {
+                throw new SQLException("Erro ao conectar: " + e.getMessage());
+            }
+        }
+        return conexao;
     }
 }
